05e7a77157008d299efe3c4032b590c30f9fc369
Devesh <devesh12200@gmail.com>
2022-11-07 11:41:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ function App() {
       return ()=>{
         document.removeEventListener("keypress", handler)
       }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [guessedLetters])
     
   return (
